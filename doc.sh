08a06831e324641be5ad3d5b6e11776ea75408fc
Andy Chu <andy@chubot.org>
2014-02-16 21:14:44 -0800

    Refining the shell.

--- doc.sh
+++ doc.sh
@@ -3,6 +3,22 @@
 # Usage:
 #   ./doc.sh <function name>
 
+# Add the HTML shell.  A data dictionary should be readable stdin.  Does NOT
+# depend on $PWD, so callers can cd.
+
+to-html() {
+  local name=$1
+  jsont doc/html.jsont > _tmp/$name-full.html
+}
+
+make-dict() {
+  local name="$1"
+  # Add PULP_ variables from the environment, so that 'PULP_latch=1 poly build'
+  pin \
+    --title-tag=h1 \
+    _tmp/$name.html
+}
+
 main() {
   local in=$1
   local out=$2
@@ -32,4 +48,6 @@ EOF
   ls -al $out
 }
 
-main "$@"
+#main "$@"
+
+"$@"
