990e33af851172ea3d79e591bde52af554d0eb50
Andy C <andy@chubot.org>
2014-02-18 15:13:04 -0800

    Use the program name as the prefix.

--- common/util.py
+++ common/util.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python
 """
-common.py
+util.py
 """
 
+import os
 import sys
 
+basename = os.path.basename(sys.argv[0])
+prefix, _ = os.path.splitext(basename)
 
 def log(msg, *args):
   if args:
     msg = msg % args
-  print >>sys.stderr, 'webpipe:', msg
-
+  print >>sys.stderr, prefix + ': ' + msg
 
