5fdaceef171f0fc64ff817043c8aa4351b806279
Andy C <andy@chubot.org>
2014-03-21 14:05:29 -0700

    Rename file2html -> xrender

---
+++ webpipe/xrender_test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python -S
+"""
+xrender_test.py: Tests for xrender.py
+"""
+
+import unittest
+
+import xrender  # module under test
+
+CSV = """\
+name,age
+<carol>,10
+<dave>,20
+"""
+
+class FunctionsTest(unittest.TestCase):
+
+  def testRenderCsv(self):
+    html, orig = xrender.RenderCsv('dir/foo.csv', 'foo.csv', CSV)
+    print html
+
+  def testGuessFileType(self):
+    self.assertEqual('png', xrender.GuessFileType('Rplot001.png'))
+    self.assertEqual('ansi', xrender.GuessFileType('typescript'))
+
+  def testCleanFilename(self):
+    print xrender.CleanFilename('foo-bar_baz')
+    print xrender.CleanFilename('foo bar')
+    print xrender.CleanFilename('foo bar <>&')
+    print xrender.CleanFilename('foo bar \\ @ ')
+
+
+class ResourcesTest(unittest.TestCase):
+
+  def testResources(self):
+    res = xrender.Resources()
+
+    p = res.GetPluginBin('ansi')
+    print p
+
+    p = res.GetPluginBin('unknown')
+    print p
+
+
+if __name__ == '__main__':
+  unittest.main()
