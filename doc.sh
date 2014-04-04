5d8255965903a72c2be6cd8bacd888f9de6c6a35
Andy C <andy@chubot.org>
2014-04-04 16:26:24 -0700

    More work on gallery.
    Add file for R.

--- doc.sh
+++ doc.sh
@@ -19,7 +19,8 @@ make-dict() {
   pin --title-tag=h1 $body_filename
 }
 
-# Build main docs
+# Build main docs.  Called by ./run.sh latch-demo, which calls './latch.sh
+# rebuild'.
 main() {
   local in=$1
   local out=$2
@@ -38,26 +39,80 @@ main() {
   ls -al $out
 }
 
+# TODO: Just list the plugins/ dir?
+plugin-types() {
+  echo txt html markdown json treemap zip
+  # TODO: csv has issue with importing JSON Template; not being hermetic
+  # R generates <html>, it's not a real snippet.
+}
+
 # Generate a named snippet for each plugin type.  Then join them into an HTML
 # doc.
-gallery() {
-  local base=$PWD/_tmp/gallery  # absolute path
+
+gallery-snippets() {
+  local plugin_types="$1"
+  local base=$2
+
   rm -rf $base/out  # so it's numbered from 1
   mkdir -p $base/in $base/out
 
-  for p in txt html; do
+  for p in $plugin_types; do
     local plugin=$PWD/plugins/$p/render 
     local in=$PWD/plugins/$p/testdata/tiny.$p
     # NOTE: we are not providing a number
-    local out=$base/out/$p
+    local out=$p
 
     # plugins are written to be in the output dir.
     pushd $base/out
     $plugin $in $out
-    popd
+    popd >/dev/null
+  done
+}
+
+print-gallery() {
+  local plugin_types="$1"
+  local base=$2
+
+  cat <<EOF
+<h1>webpipe Gallery</h1>
+
+<p>Here is a list of file types and example documents.  Click through.</p>
+
+EOF
+
+  # Generate TOC.
+  for p in $plugin_types; do
+    echo "<a href=\"#$p\">$p</a> <br/>"
   done
 
-  ls -al $base/out
+  # Generate snippets.  TODO: should be use a <div> or something?
+
+  for p in $plugin_types; do
+    echo "<a id=\"$p\"><h3>$p</h3> </a>"
+    # snippet inline
+    cat $base/out/$p.html
+  done
+}
+
+# TODO: how to get the .js to work?
+# It goes ../../...
+# Maybe it should be a variable, like $WEBPIPE_PLUGIN_BASE_URL.
+
+gallery() {
+  local plugin_types="$(plugin-types)"
+  local base_dir=$PWD/_tmp/gallery  # absolute path
+
+  gallery-snippets "$plugin_types" $base_dir
+
+  local body=$base_dir/out/body.html
+  local out=$base_dir/out/index.html
+  print-gallery "$plugin_types" $base_dir > $body
+
+  # NOTE: It's interesting that #anchors don't work without <html>?  At least
+  # in Chrome.
+  make-dict $body | to-html $out
+
+  ls -al $base_dir/out
 }
 
 "$@"
---
+++ plugins/R/testdata/tiny.R
@@ -0,0 +1,5 @@
+#!/usr/bin/Rscript
+
+print("hi")
+print(rnorm(10))
+
