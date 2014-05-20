79ac6db5dce17bc9ee85d64dba05676238c0ad78
Andy C <andy@chubot.org>
2014-05-20 02:40:09 -0700

    More notes about interface.

--- live-plugins/demo.sh
+++ live-plugins/demo.sh
@@ -8,6 +8,31 @@
 # Is netcat reliable?  Yeah it seems to work better for this "stream" use case.
 # Still use it as a client only though.
 #
+# PLUGIN INTERFACE
+#
+# The plugins need to send the initial HTML and JS as they always do.
+# (NOTE: right now the server directly reads the JS; it isn't sent)
+#
+# NOTE: This isn't possible for static publishing... only live a live server.
+# (although a crazy idea is if you load a JS file that replays it somehow)
+#
+# webpipe server should listen on an auxiliary port perhaps.
+# And then every live plugin will make a new connection on that port, send its
+# ID?  
+#
+# And then the server has to deliver those to different JS clients.
+# It probably needs some kind of ID.
+#
+# NOTE: This should possibly be a different server than webpipe.  The model is
+# different; it's not compatible with static web hosting.
+#
+# And you probably don't want a "scroll".  You probably want a page of live
+#
+# And you need a different protocol than just a hanging get.  Although I guess
+# you can dispatch by URL.  Every plugin can send the URL it wants?
+#
+# 8901 - jspipe?  This name is too common.  livepipe?
+#
 # Usage:
 #   ./demo.sh <function name>
 
